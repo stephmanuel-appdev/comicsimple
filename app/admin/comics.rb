c23f738cb5d303809ff936d9fc502be22c82b28f
Raghu Betina <raghu@firstdraft.com>
2018-06-07 00:56:08 +0000

    Whitelist columns in Comic for ActiveAdmin

--- app/admin/comics.rb
+++ app/admin/comics.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Comic do
+
+ permit_params :name, :comic_url
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
