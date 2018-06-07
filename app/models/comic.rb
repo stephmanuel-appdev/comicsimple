b17257a808366ecd6547064893a03eac179b0859
Raghu Betina <raghu@firstdraft.com>
2018-06-07 00:55:49 +0000

    Generate comics resource with starter:resource

---
+++ app/models/comic.rb
@@ -0,0 +1,8 @@
+class Comic < ApplicationRecord
+  # Direct associations
+
+  # Indirect associations
+
+  # Validations
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+  # Routes for the Comic resource:
+  # CREATE
+  get "/comics/new", :controller => "comics", :action => "new"
+  post "/create_comic", :controller => "comics", :action => "create"
+
+  # READ
+  get "/comics", :controller => "comics", :action => "index"
+  get "/comics/:id", :controller => "comics", :action => "show"
+
+  # UPDATE
+  get "/comics/:id/edit", :controller => "comics", :action => "edit"
+  post "/update_comic/:id", :controller => "comics", :action => "update"
+
+  # DELETE
+  get "/delete_comic/:id", :controller => "comics", :action => "destroy"
+  #------------------------------
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
