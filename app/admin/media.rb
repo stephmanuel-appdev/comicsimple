de23e02f919d519b8eb6679979d3395b0bedc440
Raghu Betina <raghu@firstdraft.com>
2018-06-07 00:56:10 +0000

    Whitelist columns in Medium for ActiveAdmin

--- app/admin/media.rb
+++ app/admin/media.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Medium do
+
+ permit_params :comic_id, :name, :url, :description, :social_issue
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
